b2ca51b85f57895603353de5d1837357ea865c05
D3m0l1d0r <carlos.henrique-fr@hotmail.com>
2016-02-05 22:56:28 -0200

    Update Poseidon.py

--- Poseidon.py
+++ Poseidon.py
@@ -17,36 +17,35 @@ def System():
 		system("clear")
 
 def Banner():
-	print """\n\033[1;32m              
-	                       .                                                      .
-        .n                   .                 .                  n.
-  .   .dP                  dP                   9b                 9b.    .
- 4    qXb         .       dX                     Xb       .        dXp     t
-dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
-9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
- 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
-  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
-    `9XXXXXXXXXXXP' `9XX'          `98v8P'          `XXP' `9XXXXXXXXXXXP'
-        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
-                        )b.  .dbo.dP'`v'`9b.odb.  .dX(
-                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
-                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
-                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
-                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP
-                     `'      9XXXXXX(   )XXXXXXP      `'
-                              XXXX X.`v'.X XXXX
-                              XP^X'`b   d'`X^XX
-                              X. 9  `   '  P )X
-                              `b  `       '  d'
-                               `             ' 
-              
-                  \033[0;36m- Coded By D3m0l1d0r & Pedro Souza -\033[0m 
+	print """\n             
 
-\n\033[0m""" 
+               . '@(@@@@@@@)@. (@@) `  .   '
+     .  @@'((@@@@@@@@@@@)@@@@@)@@@@@@@)@ 
+     @@(@@@@@@@@@@))@@@@@@@@@@@@@@@@)@@` .
+  @.((@@@@@@@)(@@@@@@@@@@@@@@))@\@@@@@@@@@)@@@  .
+ (@@@@@@@@@@@@@@@@@@)@@@@@@@@@@@\\@@)@@@@@@@@)
+(@@@@@@@@)@@@@@@@@@@@@@(@@@@@@@@//@@@@@@@@@) ` 
+ .@(@@@@)##&&&&&(@@@@@@@@)::_=(@\\@@@@)@@ .   .'
+   @@`(@@)###&&&&&!!;;;;;;::-_=@@\\@)@`@.
+   `   @@(@###&&&&!!;;;;;::-=_=@.@\\@@     '
+      `  @.#####&&&!!;;;::=-_= .@  \\
+            ####&&&!!;;::=_-        `
+             ###&&!!;;:-_=
+              ##&&!;::_=
+             ##&&!;:=
+            ##&&!:-
+           #&!;:-
+          #&!;= 
+          #&!- 
+           #&=
+           #&-
+            \\#/'
+ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+            - Coded By D3m0l1d0r - \n""" 
 
 def Seach():
-	target =  raw_input("\033[0;37mTarget =:>\033[0m")
-	word = raw_input("\033[0;37mWordList =:>\033[0m")
+	target =  raw_input("Target =:>")
+	word = raw_input("Wordlist =:>")
 	wordp = open(word, "r").readlines()
 
 	for i in wordp:
@@ -55,9 +54,9 @@ def Seach():
 		if seach.getcode() == 200:
 			seach = seach.read()
 			if 'Login' or 'Senha' in seach:
-				print "\033[0;32m[+] Encontrada: "+target+i+"\033[0m"
+				print "[+] Encontrada: "+target+i+""
 		else:
-			print "\033[0;31m[-] Não Encontrada : "+target+i+"\033[0m"
+			print "[-] Não Encontrada : "+target+i+""
 
 
 
